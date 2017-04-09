103037ab6d291a0e5277ce134cd02b0eb2f6f9ec
Marco Bonelli <mb5.marcob@gmail.com>
2017-04-09 02:01:58 +0200

    Edited `test.c` aadding tests for `mbc_raw_to_hex` and `mbc_hex_to_raw`. `raw_to_hex` doesn't seem to work, needs debugging.

--- test.c
+++ test.c
@@ -15,25 +15,54 @@ int main(int argc, char* argv[]) {
 	user_key = argv[2];
 	user_key_len = strlen(user_key);
 
+	mbc_set_user_key(user_key, user_key_len);
+	encoded = mbc_encode(user_data, user_data_len);
+	decoded = mbc_decode(encoded, user_data_len);
 
+	printf("Testing encoding/decoding:\n");
 	for (i = 0; i < user_data_len; i++)
 		printf("%2x ", user_data[i]);
 	printf("\n");
-
-	mbc_set_user_key(user_key, user_key_len);
-	encoded = mbc_encode(user_data, user_data_len);
 
 	for (i = 0; i < user_data_len; i++)
 		printf("%2x ", encoded[i]);
 	printf("\n");
 
-	decoded = mbc_decode(encoded, user_data_len);
-
 	for (i = 0; i < user_data_len; i++)
 		printf("%2x ", decoded[i]);
 	printf("\n");
 
+	uint8_t test_raw[] = {0x10, 0xff, 0xaa, 0x01, 0x11};
+	char test_hex[] = "00fFAA0111";
+	size_t test_raw_size, test_hex_size, raw_result_size;
+	uint8_t* raw_result;
+	char* hex_result;
+
+	test_raw_size = sizeof(test_raw);
+	test_hex_size = strlen(test_hex);
+
+	hex_result = mbc_raw_to_hex(test_raw, test_raw_size, false);
+	raw_result = mbc_hex_to_raw(test_hex, &raw_result_size);
+
+	printf("\nTesting conversion:\n   Raw: ");
+	for (i = 0; i < test_raw_size; i++)
+		printf("%2x", test_raw[i]);
+	printf("\n");
+
+	printf("To hex: ");
+	printf("%s\n", hex_result);
+
+	printf("   Hex: ");
+	printf("%s\n", test_hex);
+
+	printf("To raw: ");
+	for (i = 0; i < raw_result_size; i++)
+		printf("%2x", raw_result[i]);
+	printf("\n");
+
 	mbc_free();
+	free(raw_result);
+	free(hex_result);
 
 	return 0;
 }
