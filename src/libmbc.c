e87c77a68f851135c65915d2d6fc06547d7a4c6f
Matteo Bernardini <mttbernardini@users.noreply.github.com>
2017-04-11 18:53:55 +0200

    Using inplace decoding for `mbc_decode_from_hex`.

--- src/libmbc.c
+++ src/libmbc.c
@@ -213,20 +213,17 @@ char* mbc_encode_to_hex(const uint8_t* raw_in, size_t raw_size, bool uppercase) 
 }
 
 uint8_t* mbc_decode_from_hex(const char* hex_in, size_t* decoded_size_ptr) {
-	uint8_t *raw_data, *decoded_data;
+	uint8_t *raw_data;
 	size_t raw_size;
 
 	raw_data = mbc_hex_to_raw(hex_in, &raw_size);
 	if (raw_data == NULL)
 		return NULL;
 
-	decoded_data = mbc_decode(raw_data, raw_size);
-	if (decoded_data == NULL)
-		return NULL;
-
+	mbc_decode_inplace(raw_data, raw_size);
 	*decoded_size_ptr = raw_size;
 
-	return decoded_data;
+	return raw_data;
 }
 
 char* mbc_encode_hex2hex(const char* hex_in, bool uppercase) {
