a6ca6c8f0e8e04e4b060203342a4f328655b1c11
Marco Bonelli <mb5.marcob@gmail.com>
2017-04-18 18:44:48 +0200

    Edited post conditions on `hex_to_raw` and `raw_to_hex` to be more precise.

--- src/libmbc.h
+++ src/libmbc.h
@@ -66,7 +66,7 @@ uint8_t* mbc_decode_from_hex(const char* hex_in, size_t* decoded_size_ptr);
  * Converts raw data into an hexadecimal string.
  * @ret  NULL-terminated hexadecimal string, `NULL` if the string cannot be `malloc`ated.
  * @pre  `raw` is an array of bytes; `raw_size > 0`.
- * @post The length of the returned string is even, containing only lower/uppercase (accordingly to `uppercase`) hexadecimal ASCII characters.
+ * @post The length of the returned string is double the size of the `raw` array, containing only lower/uppercase (accordingly to `uppercase`) hexadecimal ASCII characters.
  */
 char* mbc_raw_to_hex(const uint8_t* raw, size_t raw_size, bool uppercase);
 
@@ -75,7 +75,7 @@ char* mbc_raw_to_hex(const uint8_t* raw, size_t raw_size, bool uppercase);
  * @ret  Raw data array, `NULL` if the array cannot be `malloc`ated.
  * @pre  `hex` is a non-empty NULL-terminated string containing only lower/uppercase hexadecimal ASCII characters.
  *       If the length of the string is not even the last extra char is discarded.
- * @post `*raw_size_ptr` contains the size of the raw data returned.
+ * @post `*raw_size_ptr` contains the size of the raw data returned, which is exactly half the length of the `hex` string.
  */
 uint8_t* mbc_hex_to_raw(const char* hex, size_t* raw_size_ptr);
 
