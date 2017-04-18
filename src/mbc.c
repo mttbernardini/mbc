ec8af532792e083f09d486dfd5b9555a83a57713
Marco Bonelli <mb5.marcob@gmail.com>
2017-04-19 01:09:53 +0200

    CLI program rework.  - Added function prototypes and moved functions bodies after main.  - Simplified getopt logic.  - Modified logs for help and version: better formatting and readability.

--- src/mbc.c
+++ src/mbc.c
@@ -6,12 +6,15 @@
 #include <string.h>
 #include <libmbc.h>
 
-static const char* CLI_VERSION = "0.1";
-static char* CLI_NAME; // will be set by main()
-static const char* USAGE_OPT = "(-e | -d) -k key [-x] [-v] [-h]";
+#define MBC_VERSION "0.1"
+
+static const char* VERSION_INFO = "MBC version " MBC_VERSION "\nCopyright (c) 2017 Matteo Bernardini & Marco Bonelli.\n";
+static const char* USAGE_INFO = "(-e | -d) -k key [-x] [-v] [-h]";
+static const char* SHORT_DESC =
+	"MBC is a quick tool for encoding/decoding data from stdio using the libmbc\n"
+	"library, which uses the Mattyw&MeBeiM Symmetric Encryption Algorithm.\n"
+	"Source code available at https://github.com/mttbernardini/mbc.\n";
 static const char* HELP_MSG =
-	"mbc is a quick tool for encoding/decoding data from stdio using the libmbc\n"
-	"library, which uses the Mattyw&MeBeiM Symmetric Encryption Algorithm.\n\n"
 	"OPTIONS:\n"
 	" -e        Encodes data from stdio and outputs it to stdout.\n"
 	" -d        Decodes data from stdio and outputs it to stdout.\n"
@@ -26,7 +29,95 @@ static const char* HELP_MSG =
 static const size_t RAW_CHUNK_SIZE = 32 << 20;
 static const size_t HEX_CHUNK_SIZE = 64 << 20;
 
-void mbc_core(bool do_enc, const char* user_key, bool hex_mode) {
+static char* CLI_NAME;
+
+void print_version();
+void print_usage();
+void print_help();
+void print_invalid();
+void mbc_core(bool enc_mode, bool hex_mode, const char* user_key);
+
+int main(int argc, char* argv[]) {
+	char opt;
+	char* key;
+	bool enc, hex, mode_set;
+
+	CLI_NAME = argv[0];
+
+	key = NULL;
+	hex = false;
+
+	while ((opt = getopt(argc, argv, "edk:xvh")) != -1) {
+		switch (opt) {
+			case 'e':
+				if (!mode_set) {
+					enc = true;
+					mode_set = true;
+				} else {
+					print_invalid();
+					return 1;
+				}
+
+				break;
+			case 'd':
+				if (!mode_set) {
+					enc = false;
+					mode_set = true;
+				} else {
+					print_invalid();
+					return 1;
+				}
+
+				break;
+			case 'k':
+				key = optarg;
+				break;
+			case 'x':
+				hex = true;
+				break;
+			case 'h':
+				print_help();
+				return 0;
+			case 'v':
+				print_version();
+				return 0;
+			default:
+				print_usage();
+				return 1;
+		}
+	}
+
+	if (!mode_set || key == NULL) {
+		print_invalid();
+		return 1;
+	}
+
+	mbc_core(enc, hex, key);
+
+	return 0;
+}
+
+void print_version() {
+	fprintf(stderr, "%s", VERSION_INFO);
+}
+
+void print_usage() {
+	fprintf(stderr, "Usage: %s %s\n", CLI_NAME, USAGE_INFO);
+}
+
+void print_help() {
+	print_version();
+	fprintf(stderr, "\n%s\n", SHORT_DESC);
+	print_usage();
+	fprintf(stderr, "\n%s", HELP_MSG);
+}
+
+void print_invalid() {
+	fprintf(stderr, "Please set a valid mode (-d OR -e) and a key (-k).\n");
+	print_usage();
+}
+
+void mbc_core(bool enc_mode, bool hex_mode, const char* user_key) {
 	uint8_t *buffer_in_raw, *buffer_out_raw;
 	char *buffer_in_hex, *buffer_out_hex;
 	size_t user_key_len, bytes_read, bytes_to_write;
@@ -35,7 +126,7 @@ void mbc_core(bool do_enc, const char* user_key, bool hex_mode) {
 	mbc_set_user_key((uint8_t*) user_key, user_key_len);
 
 	if (hex_mode) {
-		if (do_enc) {
+		if (enc_mode) {
 
 			buffer_in_raw = malloc(RAW_CHUNK_SIZE);  //TODO: handle malloc error
 
@@ -59,7 +150,7 @@ void mbc_core(bool do_enc, const char* user_key, bool hex_mode) {
 
 		buffer_in_raw = malloc(RAW_CHUNK_SIZE);  //TODO: handle malloc error
 
-		if (do_enc) {
+		if (enc_mode) {
 			while ((bytes_read = fread(buffer_in_raw, 1, RAW_CHUNK_SIZE, stdin))) {
 				mbc_encode_inplace(buffer_in_raw, bytes_read);
 				fwrite(buffer_in_raw, 1, bytes_read, stdout);
@@ -75,80 +166,4 @@ void mbc_core(bool do_enc, const char* user_key, bool hex_mode) {
 	}
 
 	mbc_free();
-}
-
-void print_version() {
-	fprintf(stderr, "%s %s\n", CLI_NAME, CLI_VERSION);
-}
-
-void print_usage() {
-	fprintf(stderr, "Usage: %s %s\n", CLI_NAME, USAGE_OPT);
-}
-
-void print_help() {
-	print_version();
-	print_usage();
-	fprintf(stderr, "\n%s", HELP_MSG);
-}
-
-void print_invalid() {
-	fprintf(stderr, "Please set a valid mode (-d or -e) and a key (-k).\n");
-	print_usage();
-}
-
-int main(int argc, char* argv[]) {
-	char opt;
-	char* key;
-	bool enc_flag, dec_flag, mode_set, hex_mode;
-
-	key = NULL;
-	hex_mode = false;
-	enc_flag = false;
-	dec_flag = false;
-	mode_set = false;
-
-	CLI_NAME = argv[0];
-
-	while ((opt = getopt(argc, argv, "edk:xvh")) != -1) {
-		switch (opt) {
-			case 'e':
-				if (!dec_flag) {
-					enc_flag = true;
-					mode_set = true;
-				}
-				break;
-			case 'd':
-				if (!enc_flag) {
-					dec_flag = true;
-					mode_set = true;
-				}
-				break;
-			case 'k':
-				key = optarg;
-				break;
-			case 'x':
-				hex_mode = true;
-				break;
-			case 'h':
-				print_help();
-				return 0;
-			case 'v':
-				print_version();
-				return 0;
-			default:
-				print_usage();
-				return 1;
-		}
-	}
-
-	if (!mode_set || key == NULL) {
-		print_invalid();
-		return 1;
-	}
-
-	// fprintf(stderr, "debug: Will encode? %d, hex? %d, key? %s\n\n", enc_flag, hex_mode, key);
-
-	mbc_core(enc_flag, key, hex_mode);
-
-	return 0;
 }
