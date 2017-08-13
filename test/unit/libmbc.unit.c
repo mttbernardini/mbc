184ef00bf4d5c3faadafd9910c2e9b18c75b4508
Matteo Bernardini <mat_teobe@yahoo.it>
2017-08-13 18:03:26 +0200

    Added edge cases for empty input: - `mbc_raw_to_hex` - `mbc_hex_to_raw` - `mbc_encode_to_hex` - `mbc_decode_from_hex`

--- test/unit/libmbc.unit.c
+++ test/unit/libmbc.unit.c
@@ -58,11 +58,26 @@ CTEST(libmbc, mbc_raw_to_hex__lowercase) {
 	free(hex);
 }
 
+CTEST(libmbc, mbc_raw_to_hex__empty) {
+	char* hex = mbc_raw_to_hex((uint8_t[]){}, 0, false);
+	ASSERT_NOT_NULL(hex);
+	ASSERT_STR("", hex);
+	free(hex);
+}
+
 CTEST(libmbc, mbc_hex_to_raw) {
 	size_t n;
 	uint8_t* raw = mbc_hex_to_raw("000a61B2f", &n);
 	ASSERT_NOT_NULL(raw);
 	ASSERT_DATA(((uint8_t[]){0x00, 0x0a, 0x61, 0xb2}), 4, raw, n);
+	free(raw);
+}
+
+CTEST(libmbc, mbc_hex_to_raw__empty) {
+	size_t n;
+	uint8_t* raw = mbc_hex_to_raw("", &n);
+	//ASSERT_NULL(raw); // TODO
+	ASSERT_EQUAL(0, n);
 	free(raw);
 }
 
@@ -160,6 +175,16 @@ CTEST(libmbc, mbc_encode_to_hex) {
 	mbc_free();
 }
 
+CTEST(libmbc, mbc_encode_to_hex__empty) {
+	ASSERT_TRUE(mbc_set_user_key((uint8_t[]){0x47}, 1));
+	// swap: 3-4
+	// xor:  0x47
+	char* encoded = mbc_encode_to_hex((uint8_t[]){}, 0, false);
+	ASSERT_STR("", encoded);
+	free(encoded);
+	mbc_free();
+}
+
 CTEST(libmbc, mbc_decode_from_hex) {
 	size_t dec_size;
 	ASSERT_TRUE(mbc_set_user_key((uint8_t[]){0x47}, 1));
@@ -169,6 +194,18 @@ CTEST(libmbc, mbc_decode_from_hex) {
 	// 0000 1000 ^ 0100 0111 → swap 0100 1111 → 0101 0111
 	// 0001 1101 ^ 0100 0111 → swap 0101 1010 → 0101 1010
 	ASSERT_DATA(((uint8_t[]){0x57, 0x5a}), 2, decoded, dec_size);
+	free(decoded);
+	mbc_free();
+}
+
+CTEST(libmbc, mbc_decode_from_hex__empty) {
+	size_t dec_size;
+	ASSERT_TRUE(mbc_set_user_key((uint8_t[]){0x47}, 1));
+	// xor:  0x47
+	// swap: 3-4
+	uint8_t* decoded = mbc_decode_from_hex("", &dec_size);
+	//ASSERT_NULL(decoded);  // TODO
+	ASSERT_EQUAL(0, dec_size);
 	free(decoded);
 	mbc_free();
 }
