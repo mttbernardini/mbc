90849fe1fcd36cc6019d2985b10c9d57cb049923
Matteo Bernardini <mat_teobe@yahoo.it>
2017-08-26 18:32:50 +0200

    Consistency for random key/data generation on integration test for 'libmbc'.

--- test/integration/libmbc.int.c
+++ test/integration/libmbc.int.c
@@ -6,56 +6,67 @@
 #include <ctest.h>
 #include <libmbc.h>
 
-static const size_t MIN_KEY_SIZE = 32L;
-static const size_t MAX_KEY_SIZE = 1024L * 1024L * 32L;
+static const size_t MIN_KEY_SIZE      = 32L;
+static const size_t MAX_KEY_SIZE      = 1024L * 1024L * 32L;
 static const size_t MIN_RAW_DATA_SIZE = 128L;
 static const size_t MAX_RAW_DATA_SIZE = 1024L * 1024L * 128L;
-static const char HEX_CHARS_UPPER[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-static const char HEX_CHARS_LOWER[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+static const char   HEX_CHARS_UPPER[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+static const char   HEX_CHARS_LOWER[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-static void random_test_data(uint8_t** key, void* data, void* expected, size_t* key_size, size_t* data_size, bool hex, bool uppercase) {
+static uint8_t* generate_random_key(size_t* key_size) {
 	size_t i;
+	uint8_t* key;
 
 	if (*key_size == 0)
 		*key_size = (size_t)(rand() % (MAX_KEY_SIZE - MIN_KEY_SIZE) + MIN_KEY_SIZE + 1);
 
+	CTEST_LOG("key: %zuB", *key_size);
+
+	key = malloc(*key_size);
+	for (i = 0; i < *key_size; i++)
+		key[i] = (uint8_t)(rand() % 0xFF);
+
+	return key;
+}
+
+static void* generate_random_data(void* expected, size_t* data_size, bool hex, bool uppercase) {
+	register size_t i;
+
 	if (*data_size == 0)
 		*data_size = (size_t)(rand() % (MAX_RAW_DATA_SIZE - MIN_RAW_DATA_SIZE) + MIN_RAW_DATA_SIZE + 1);
 
-	CTEST_LOG("key: %zuB; data (%s): %zuB.", *key_size, hex ? (uppercase ? "HEX" : "hex") : "raw", *data_size);
-
-	*key = malloc(*key_size);
-	for (i = 0; i < *key_size; i++)
-		(*key)[i] = (uint8_t)(rand() % 0xFF);
+	CTEST_LOG("data (%s): %zuB.", hex ? (uppercase ? "HEX" : "hex") : "raw", *data_size);
 
 	if (hex) {
-		*(char**)data = malloc(*data_size + 1);
+		const char* HEX_CHARS = uppercase ? HEX_CHARS_UPPER : HEX_CHARS_LOWER;
+		char *hex_data, *hex_exp;
 
-		if (uppercase)
-			for (i = 0; i < *data_size; i++)
-				(*(char**)data)[i] = HEX_CHARS_UPPER[rand() % 0x10];
-		else
-			for (i = 0; i < *data_size; i++)
-				(*(char**)data)[i] = HEX_CHARS_LOWER[rand() % 0x10];
-
-		(*(char**)data)[*data_size] = '\0';
-
-		if (*data_size % 2 == 0) {
-			*(char**)expected = malloc(*data_size + 1);
-			memcpy(*(char**)expected, *(char**)data, *data_size + 1);
-		} else {
-			*(char**)expected = malloc(*data_size);
-			memcpy(*(char**)expected, *(char**)data, *data_size - 1);
-			(*(char**)expected)[*data_size - 1] = '\0';
-		}
-	} else {
-		*(uint8_t**)data     = malloc(*data_size);
-		*(uint8_t**)expected = malloc(*data_size);
+		hex_data = malloc(*data_size + 1);
+		hex_exp  = malloc(*data_size + 1);
 
 		for (i = 0; i < *data_size; i++)
-			(*(uint8_t**)data)[i] = (uint8_t)(rand() % 0xFF);
+			hex_data[i] = HEX_CHARS[rand() % 0x10];
 
-		memcpy(*(uint8_t**)expected, *(uint8_t**)data, *data_size);
+		hex_data[*data_size] = '\0';
+		memcpy(hex_exp, hex_data, *data_size + 1);
+
+		if (*data_size % 2 != 0)
+			hex_exp[*data_size - 1] = '\0';
+
+		*(char**)expected = hex_exp;
+		return hex_data;
+
+	} else {
+		uint8_t *raw_data, *raw_exp;
+		raw_data = malloc(*data_size);
+		raw_exp  = malloc(*data_size);
+
+		for (i = 0; i < *data_size; i++)
+			raw_data[i] = (uint8_t)(rand() % 0xFF);
+
+		memcpy(raw_exp, raw_data, *data_size);
+		*(uint8_t**) expected = raw_exp;
+		return raw_data;
 	}
 }
 
@@ -64,7 +75,8 @@ CTEST(libmbc, encode_decode__inplace) {
 	size_t ks, ds;
 
 	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	k = generate_random_key(&ks);
+	d = generate_random_data(&e, &ds, false, false);
 
 	mbc_set_user_key(k, ks);
 	mbc_encode_inplace(d, ds);
@@ -82,7 +94,8 @@ CTEST(libmbc, encode_decode) {
 	size_t ks, ds;
 
 	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	k = generate_random_key(&ks);
+	d = generate_random_data(&e, &ds, false, false);
 
 	mbc_set_user_key(k, ks);
 	de = mbc_encode(d, ds);
@@ -103,7 +116,8 @@ CTEST(libmbc, encode_decode__to_from_hex__lower) {
 	size_t ks, ds, dds;
 
 	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	k = generate_random_key(&ks);
+	d = generate_random_data(&e, &ds, false, false);
 
 	mbc_set_user_key(k, ks);
 	dex = mbc_encode_to_hex(d, ds, false);
@@ -124,7 +138,8 @@ CTEST(libmbc, encode_decode__to_from_hex__upper) {
 	size_t ks, ds, dds;
 
 	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	k = generate_random_key(&ks);
+	d = generate_random_data(&e, &ds, false, false);
 
 	mbc_set_user_key(k, ks);
 	dex = mbc_encode_to_hex(d, ds, true);
@@ -140,18 +155,17 @@ CTEST(libmbc, encode_decode__to_from_hex__upper) {
 }
 
 CTEST(libmbc, convert__raw_to_hex_to_raw__lower) {
-	uint8_t *k, *d, *dc, *e;
+	uint8_t *d, *dc, *e;
 	char *dx;
-	size_t ks, ds, dcs;
+	size_t ds, dcs;
 
-	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	ds = 0;
+	d = generate_random_data(&e, &ds, false, false);
 
 	dx = mbc_raw_to_hex(d, ds, false);
 	dc = mbc_hex_to_raw(dx, &dcs);
 	ASSERT_DATA(e, ds, dc, dcs);
 
-	free(k);
 	free(d);
 	free(dc);
 	free(dx);
@@ -159,18 +173,17 @@ CTEST(libmbc, convert__raw_to_hex_to_raw__lower) {
 }
 
 CTEST(libmbc, convert__raw_to_hex_to_raw__upper) {
-	uint8_t *k, *d, *dc, *e;
+	uint8_t *d, *dc, *e;
 	char *dx;
-	size_t ks, ds, dcs;
+	size_t ds, dcs;
 
-	ks = 0; ds = 0;
-	random_test_data(&k, &d, &e, &ks, &ds, false, false);
+	ds = 0;
+	d = generate_random_data(&e, &ds, false, false);
 
 	dx = mbc_raw_to_hex(d, ds, true);
 	dc = mbc_hex_to_raw(dx, &dcs);
 	ASSERT_DATA(e, ds, dc, dcs);
 
-	free(k);
 	free(d);
 	free(dc);
 	free(dx);
@@ -178,18 +191,17 @@ CTEST(libmbc, convert__raw_to_hex_to_raw__upper) {
 }
 
 CTEST(libmbc, convert__hex_to_raw_to_hex__lower) {
-	uint8_t *k, *dr;
+	uint8_t *dr;
 	char *dx, *dxc, *ex;
-	size_t ks, dxs, drs;
+	size_t dxs, drs;
 
-	ks = 0; dxs = 0;
-	random_test_data(&k, &dx, &ex, &ks, &dxs, true, false);
+	dxs = 0;
+	dx = generate_random_data(&ex, &dxs, true, false);
 
 	dr = mbc_hex_to_raw(dx, &drs);
 	dxc = mbc_raw_to_hex(dr, drs, false);
 	ASSERT_STR(ex, dxc);
 
-	free(k);
 	free(dx);
 	free(dxc);
 	free(dr);
@@ -197,18 +209,17 @@ CTEST(libmbc, convert__hex_to_raw_to_hex__lower) {
 }
 
 CTEST(libmbc, convert__hex_to_raw_to_hex__upper) {
-	uint8_t *k, *dr;
+	uint8_t *dr;
 	char *dx, *dxc, *ex;
-	size_t ks, dxs, drs;
+	size_t dxs, drs;
 
-	ks = 0; dxs = 0;
-	random_test_data(&k, &dx, &ex, &ks, &dxs, true, true);
+	dxs = 0;
+	dx = generate_random_data(&ex, &dxs, true, true);
 
 	dr = mbc_hex_to_raw(dx, &drs);
 	dxc = mbc_raw_to_hex(dr, drs, true);
 	ASSERT_STR(ex, dxc);
 
-	free(k);
 	free(dx);
 	free(dxc);
 	free(dr);
