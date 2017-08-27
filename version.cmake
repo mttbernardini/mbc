92cd54e23e343fc47af4f495efa3af4486ceec51
Matteo Bernardini <mat_teobe@yahoo.it>
2017-08-27 16:59:05 +0200

    Adding fallback hardcoded VERSION file.

--- version.cmake
+++ version.cmake
@@ -1,15 +1,26 @@
 find_package(Git)
 
+set(GIT_DESCRIBE_EXIT 1)
+
 if (GIT_FOUND)
 	execute_process(
 		COMMAND ${GIT_EXECUTABLE} describe --always
+		RESULT_VARIABLE GIT_DESCRIBE_EXIT
 		OUTPUT_VARIABLE MBC_VERSION
 		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-	STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+)-[a-z0-9]+$" "\\1.\\2.\\3" MBC_VERSION "${MBC_VERSION}")
-	STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)$" "\\1.\\2.0" MBC_VERSION "${MBC_VERSION}")
-else()
-	set(MBC_VERSION "(unknown version)")
 endif()
+
+if(NOT GIT_DESCRIBE_EXIT EQUAL 0)
+	if (EXISTS ${ROOT}/VERSION)
+		file(STRINGS ${ROOT}/VERSION MBC_VERSION LIMIT_COUNT 1)
+	else()
+		set(MBC_VERSION "(unknown version)")
+	endif()
+endif()
+
+STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+)-[a-f0-9]+$" "\\1.\\2.\\3" MBC_VERSION "${MBC_VERSION}")
+STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)$" "\\1.\\2.0" MBC_VERSION "${MBC_VERSION}")
+STRING(REGEX REPLACE "^([a-f0-9]+)$" "#\\1" MBC_VERSION "${MBC_VERSION}")
 
 configure_file(version.c.in version.c ESCAPE_QUOTES @ONLY)
