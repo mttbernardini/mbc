562d890220f121c62449965e57f7deaa8335e846
Matteo Bernardini <mat_teobe@yahoo.it>
2017-08-27 17:59:58 +0200

    Regex simplification & fixed typo.

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@ set(GIT_DESCRIBE_EXIT 1)
 
 if (GIT_FOUND)
 	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --always
+		COMMAND ${GIT_EXECUTABLE} describe --always --long
 		RESULT_VARIABLE GIT_DESCRIBE_EXIT
 		OUTPUT_VARIABLE MBC_VERSION
 		OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -19,8 +19,7 @@ if(NOT GIT_DESCRIBE_EXIT EQUAL 0)
 	endif()
 endif()
 
-STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+)-[a-f0-9]+$" "\\1.\\2.\\3" MBC_VERSION "${MBC_VERSION}")
-STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)$" "\\1.\\2.0" MBC_VERSION "${MBC_VERSION}")
+STRING(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+)-g[a-f0-9]+$" "\\1.\\2.\\3" MBC_VERSION "${MBC_VERSION}")
 STRING(REGEX REPLACE "^([a-f0-9]+)$" "#\\1" MBC_VERSION "${MBC_VERSION}")
 
 configure_file(version.c.in version.c ESCAPE_QUOTES @ONLY)
