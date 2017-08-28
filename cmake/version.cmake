8b7d4b1ae3486eb11f74305bf715f4735b375219
Matteo Bernardini <mat_teobe@yahoo.it>
2017-08-28 23:20:41 +0200

    Removed VERSION. Version is now hardcoded on CMakeLists.txt (as fallback).

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,52 +1,41 @@
 find_package(Git)
 
-set(GIT_DESCRIBE_EXIT 1)
-set(MBC_VERSION "")
+set(MBC_VERSION "${HARD_VERSION}")
+set(MBC_REVISION "")
 
 if (GIT_FOUND)
 	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --always --long --first-parent
+		COMMAND ${GIT_EXECUTABLE} describe --always --long --first-parent --match "v[0-9]*"
 		RESULT_VARIABLE GIT_DESCRIBE_EXIT
-		OUTPUT_VARIABLE MBC_VERSION
+		OUTPUT_VARIABLE GIT_DESCRIBE_OUT
 		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
+	if (GIT_DESCRIBE_EXIT EQUAL 0)
+		# Version format conventions
+		if (GIT_DESCRIBE_OUT MATCHES "^[a-f0-9]+$") # fallback on only hash
+			set(MBC_VERSION "#dev-${GIT_DESCRIBE_OUT}")
+		else()
+			string(REGEX REPLACE "^v([0-9]+\\.[0-9]+(\\.[0-9]+)?).*" # standard version
+				"\\1" MBC_VERSION "${GIT_DESCRIBE_OUT}")
+			string(REGEX REPLACE "^v.*-([0-9]+)-g[a-f0-9]+$" # extract revision
+				"\\1" MBC_REVISION "${GIT_DESCRIBE_OUT}")
+		endif()
+		if (NOT HARD_VERSION STREQUAL "" AND NOT MBC_VERSION STREQUAL HARD_VERSION)
+			message(AUTHOR_WARNING
+				"Hardcoded version in CMakeLists.txt and `git describe` computed version do not match! Using computed version.\n"
+				"Hardcoded: ${HARD_VERSION} - Computed: ${MBC_VERSION}\n"
+			)
+		endif()
+	endif()
 endif()
 
-if((NOT GIT_DESCRIBE_EXIT EQUAL 0) AND EXISTS ${ROOT}/VERSION)
-	file(STRINGS ${ROOT}/VERSION MBC_VERSION LIMIT_COUNT 1)
+if(NOT (MBC_REVISION STREQUAL "0" OR MBC_REVISION STREQUAL ""))
+	set(MBC_VERSION "${MBC_VERSION}_r${MBC_REVISION}")
 endif()
 
 if(MBC_VERSION STREQUAL "")
 	set(MBC_VERSION "(unknown version)")
 endif()
-
-
-# Version format conventions
-STRING(REGEX REPLACE  # master standard release
-	"^v([0-9]+)\\.([0-9]+)-([0-9]+)-g[a-f0-9]+$"
-	"\\1.\\2.\\3"
-	MBC_VERSION "${MBC_VERSION}"
-)
-STRING(REGEX REPLACE  # master variant release
-	"^v([0-9]+)\\.([0-9]+)-([a-zA-Z0-9]+)-([0-9]+)-g[a-f0-9]+$"
-	"\\1.\\2.\\4-\\3"
-	MBC_VERSION "${MBC_VERSION}"
-)
-STRING(REGEX REPLACE  # master release fallback (no rev. number)
-	"^v([0-9]+)\\.([0-9]+)$"
-	"\\1.\\2"
-	MBC_VERSION "${MBC_VERSION}"
-)
-STRING(REGEX REPLACE  # master variant release fallback (no rev. number)
-	"^v([0-9]+)\\.([0-9]+)-([a-zA-Z0-9]+)$"
-	"\\1.\\2-\\3"
-	MBC_VERSION "${MBC_VERSION}"
-)
-STRING(REGEX REPLACE  # development release (commit hash based)
-	"^([a-f0-9]+)$"
-	"#git-\\1"
-	MBC_VERSION "${MBC_VERSION}"
-)
 
 message("-- MBC version: ${MBC_VERSION}")
 configure_file(version.c.in version.c ESCAPE_QUOTES @ONLY)
