e8edaf4b88ee8ccf99b797506212371818c3e099
Marco Bonelli <mb5.marcob@gmail.com>
2018-02-14 21:46:49 +0100

    Added memory fail tests.

--- test/memory/libmbc.mem.c
+++ test/memory/libmbc.mem.c
@@ -4,7 +4,30 @@
 
 #include <ctest.h>
 
-
 CTEST(libmbc, mbc_set_user_key_fail) {
-	ASSERT_FALSE(mbc_set_user_key((uint8_t[]){0x10}, 1));
+	ASSERT_FALSE(mbc_set_user_key(NULL, 1));
+}
+
+CTEST(libmbc, mbc_encode_fail) {
+	ASSERT_NULL(mbc_encode(NULL, 1));
+}
+
+CTEST(libmbc, mbc_decode_fail) {
+	ASSERT_NULL(mbc_decode(NULL, 1));
+}
+
+CTEST(libmbc, mbc_encode_to_hex_fail) {
+	ASSERT_NULL(mbc_encode_to_hex(NULL, 1, false));
+}
+
+CTEST(libmbc, mbc_decode_from_hex_fail) {
+	ASSERT_NULL(mbc_decode_from_hex("fail", NULL));
+}
+
+CTEST(libmbc, mbc_raw_to_hex_fail) {
+	ASSERT_NULL(mbc_raw_to_hex(NULL, 1, false));
+}
+
+CTEST(libmbc, mbc_hex_to_raw_fail) {
+	ASSERT_NULL(mbc_hex_to_raw("fail", NULL));
 }
