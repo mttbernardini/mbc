bd1fc04cf2f7c75bd316adbd80b00c0bfbbaa3e2
Marco Bonelli <mb5.marcob@gmail.com>
2018-02-15 01:32:21 +0100

    Better safe than sorry: use valid data in mem tests.

--- test/memory/libmbc.mem.c
+++ test/memory/libmbc.mem.c
@@ -6,35 +6,39 @@
 
 CTEST(libmbc, mbc_set_user_key_fail) {
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_FALSE(mbc_set_user_key(NULL, 1));
+	ASSERT_FALSE(mbc_set_user_key((uint8_t*)"fail", 4));
 }
 
 CTEST(libmbc, mbc_encode_fail) {
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_encode(NULL, 1));
+	ASSERT_NULL(mbc_encode((uint8_t*)"fail", 4));
 }
 
 CTEST(libmbc, mbc_decode_fail) {
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_decode(NULL, 1));
+	ASSERT_NULL(mbc_decode((uint8_t*)"fail", 4));
 }
 
 CTEST(libmbc, mbc_encode_to_hex_fail) {
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_encode_to_hex(NULL, 1, false));
+	ASSERT_NULL(mbc_encode_to_hex((uint8_t*)"fail", 4, false));
 }
 
 CTEST(libmbc, mbc_decode_from_hex_fail) {
+	size_t decoded_size;
+
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_decode_from_hex("fail", NULL));
+	ASSERT_NULL(mbc_decode_from_hex("fail", &decoded_size));
 }
 
 CTEST(libmbc, mbc_raw_to_hex_fail) {
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_raw_to_hex(NULL, 1, false));
+	ASSERT_NULL(mbc_raw_to_hex((uint8_t*)"fail", 4, false));
 }
 
 CTEST(libmbc, mbc_hex_to_raw_fail) {
+	size_t raw_size;
+
 	MEM_CONTROL_FAIL_NEXT = true;
-	ASSERT_NULL(mbc_hex_to_raw("fail", NULL));
+	ASSERT_NULL(mbc_hex_to_raw("fail", &raw_size));
 }
